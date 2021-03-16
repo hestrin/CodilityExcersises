93a5917d65a94b159353492228a80e92eaf1248f
Mateusz <matmmal@gmail.com>
2021-03-16 16:28:49 +0100

    Initial commit for codility exercises. Excercise 1: Equal sum subsets started.

---
+++ src/test/java/BaseTest.java
@@ -0,0 +1,10 @@
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class BaseTest {
+    @Test
+    void testSth() {
+        assertThat(1).isOne();
+    }
+}
